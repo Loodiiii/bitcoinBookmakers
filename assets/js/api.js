c90e69654e9f2222596b6768d4b73f78dbad9a27
PetarNovitovic <pnovitovic1@gmail.com>
2020-11-26 19:36:32 +0100

    all functionalities have been finished

---
+++ assets/js/api.js
@@ -0,0 +1,29 @@
+// API
+
+fetch("https://bitpay.com/api/rates")
+    .then(result => result.json())
+    .then(data => {
+        const btc = document.querySelector(".btc");
+        const eth = document.querySelector(".eth")
+        const bch = document.querySelector(".btCash");
+        const xrp = document.querySelector(".ripple")
+
+       data.forEach (
+            (crypto) => {
+            if(crypto.code == "USD"){
+                btc.innerHTML = "$ " + crypto.rate;
+            }
+            if(crypto.code == "ETH"){
+                eth.innerHTML = "$ " + (data[2].rate / crypto.rate).toFixed(3);
+            }
+
+            if(crypto.code == "BCH"){
+                bch.innerHTML = "$ " + (data[2].rate / crypto.rate).toFixed(3);
+            }
+            if(crypto.code == "XRP"){
+                xrp.innerHTML = "$ " + (data[2].rate / crypto.rate).toFixed(3);
+            }
+        
+        })
+
+})
