9db76a24aa412996464d3752228195fd35f9fced
PetarNovitovic <pnovitovic1@gmail.com>
2020-12-09 20:07:48 +0100

    site's being completed, adding images and links, final touches

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@ const cloud = document.querySelector(".bgd-container")
 const cloudBet = document.querySelector("#cloudBetLink")
 
 cloud.addEventListener("click", function(){
-    //firstAd();
+    firstAd();
 })
 
 cloudBet.addEventListener("click", function(){
@@ -16,3 +16,72 @@ function firstAd(){
 function cloudBetFunct(){
     window.open('https://cloudbet.com/en/bitcoin-bonus?af_token=988130567e1ffa3883f9e2c8e86fa82f', '_blank');
 }
+
+
+let item1 = document.querySelector(".item1")
+let item2 = document.querySelector(".item2")
+let item3 = document.querySelector(".item3")
+let item4 = document.querySelector(".item4")
+let item5 = document.querySelector(".item5")
+let item6 = document.querySelector(".item6")
+let item7 = document.querySelector(".item7")
+let item8 = document.querySelector(".item8")
+let item9 = document.querySelector(".item9")
+
+function betcoin(){
+    window.open('https://www.betcoin.ag/?r=297919', '_blank');
+}
+
+function betwinner(){
+    window.open("https://bwredir.com/1hpx?p=/user/registration", "_blank");
+}
+
+function megaPari(){
+    window.open("https://refpaiozdg.top/L?tag=d_750007m_25437c_BitcoinBookmakers&site=750007&ad=25437", "_blank");
+}
+
+function fortuneJack(){
+    window.open("https://refpaiozdg.top/L?tag=d_750007m_25437c_BitcoinBookmakers&site=750007&ad=25437", "_blank");
+}
+
+function betXbet(){
+    window.open("https://refpaiozdg.top/L?tag=d_750007m_25437c_BitcoinBookmakers&site=750007&ad=25437", "_blank");
+}
+
+function betmaster(){
+    window.open("https://refpaiozdg.top/L?tag=d_750007m_25437c_BitcoinBookmakers&site=750007&ad=25437", "_blank");
+}
+
+function xBitCom(){
+    window.open("https://refpaiozdg.top/L?tag=d_750007m_25437c_BitcoinBookmakers&site=750007&ad=25437", "_blank");
+}
+
+function cryptobet(){
+    window.open("https://refpaiozdg.top/L?tag=d_750007m_25437c_BitcoinBookmakers&site=750007&ad=25437", "_blank");
+}
+
+function megaPari(){
+    window.open("https://refpaiozdg.top/L?tag=d_750007m_25437c_BitcoinBookmakers&site=750007&ad=25437", "_blank");
+}
+
+item1.addEventListener("click", () => {
+
+})
+item2.addEventListener("click", megaPari);
+item3.addEventListener("click", () => {
+    
+})
+item4.addEventListener("click", () => {
+    
+})
+item5.addEventListener("click", betwinner);
+item6.addEventListener("click", () => {
+    
+})
+item7.addEventListener("click", () => {
+    
+})
+item8.addEventListener("click", betcoin);
+item9.addEventListener("click", () => {
+    
+})
